f4192bfae0f0a171466e54367d67d7c18253ebe0
Mateusz Rojek <mateuszr2047@gmail.com>
2024-02-29 19:49:43 +0100

    Utworzenie projektu

---
+++ src/main/java/pl/matipol/stronainternetowa/StronaInternetowaApplication.java
@@ -0,0 +1,13 @@
+package pl.matipol.stronainternetowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaInternetowaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaInternetowaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/matipol/stronainternetowa/StronaInternetowaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matipol.stronainternetowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StronaInternetowaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
